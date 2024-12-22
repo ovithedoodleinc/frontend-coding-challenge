72b4ed54df03fbd1c4fb0caddd77bca5db8108b9
Showvike Mondal Ovi <ovi.thedoodleinc@gmail.com>
2024-12-23 03:07:01 +0600

    Updated URL When Modal Opens (Products Page)
    Updated the URL when the modal opens on the `/products` page, ensuring the modal state persists after a page reload and works with the back/forward buttons in the browser.

--- src/views/products/products.tsx
+++ src/views/products/products.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import React, { useState, useCallback } from "react";
-import { Product } from "@/types";
-import { ProductModal } from "@/views/products/productModal/productModal";
 import { BackToHome } from "@/components/backToHome/backToHome";
-import { ProductList } from "@/views/products/productList/productList";
-import { PaginationControls } from "@/views/products/paginationControls/paginationControls";
-import { usePagination } from "@/hooks/usePagination";
 import { PRODUCTS_DATA } from "@/data/productsData";
+import { usePagination } from "@/hooks/usePagination";
+import { Product } from "@/types";
+import { PaginationControls } from "@/views/products/paginationControls/paginationControls";
+import { ProductList } from "@/views/products/productList/productList";
+import { ProductModal } from "@/views/products/productModal/productModal";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useCallback, useEffect, useState } from "react";
 
 export const Products: React.FC = () => {
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
@@ -18,13 +19,49 @@ export const Products: React.FC = () => {
     handlePageChange,
   } = usePagination({ items: PRODUCTS_DATA, itemsPerPage: 5 });
 
-  const handleOpenModal = useCallback((product: Product) => {
-    setSelectedProduct(product);
-  }, []);
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const handleOpenModal = useCallback(
+    (product: Product) => {
+      setSelectedProduct(product);
+
+      if (product?.id) {
+        router.push(`${pathname}?product-id=${product.id}`);
+      }
+    },
+    [pathname, router]
+  );
 
   const handleCloseModal = useCallback(() => {
     setSelectedProduct(null);
-  }, []);
+
+    router.push(pathname);
+  }, [pathname, router]);
+
+  const handleFindProductById = useCallback(
+    (id: string) => {
+      const product = PRODUCTS_DATA.find((item) => item.id === id);
+
+      handleOpenModal(product as Product);
+    },
+    [handleOpenModal]
+  );
+
+  const handleSearchParams = useCallback(() => {
+    if (searchParams.has("product-id")) {
+      const id = searchParams.get("product-id");
+
+      handleFindProductById(id as string);
+    } else {
+      setSelectedProduct(null);
+    }
+  }, [handleFindProductById, searchParams]);
+
+  useEffect(() => {
+    handleSearchParams();
+  }, [handleSearchParams]);
 
   return (
     <div>
